ad08e3a40289eadf6c13d0f83f8c28ff22cd925a
Seif Hassan <seif.hassan.4@gmail.com>
2021-12-23 01:03:06 -0500

    Updated Captcha

--- js/theme.js
+++ js/theme.js
@@ -59,13 +59,4 @@
 		});
 	}
 
-	$("form").each(function() {
-		$(this).find(':input[type="submit"]').prop('disabled', true);
-	});
-	function correctCaptcha() {
-		$("form").each(function() {
-			$(this).find(':input[type="submit"]').prop('disabled', false);
-		});
-	}
-
 })(jQuery);
