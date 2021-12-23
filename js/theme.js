6abc700e97d218aba6c57d534d14b49ee985b554
Seif Hassan <seif.hassan.4@gmail.com>
2021-12-23 00:10:37 -0500

    Updated Website Template
    New Website!

---
+++ js/theme.js
@@ -0,0 +1,75 @@
+(function ($) {
+	'use strict';
+
+	var nav_offset_top = $('header').height() + 50;
+	/*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+
+	//* Navbar Fixed
+	function navbarFixed() {
+		if ($('.header_area').length) {
+			$(window).scroll(function () {
+				var scroll = $(window).scrollTop();
+				if (scroll >= nav_offset_top) {
+					$('.header_area').addClass('navbar_fixed');
+				} else {
+					$('.header_area').removeClass('navbar_fixed');
+				}
+			});
+		}
+	}
+	navbarFixed();
+
+	// $('select').niceSelect();
+	/* ---------------------------------------------
+            Isotope js Starts
+         --------------------------------------------- */
+	$(window).on('load', function () {
+		$('.portfolio-filter ul li').on('click', function () {
+			$('.portfolio-filter ul li').removeClass('active');
+			$(this).addClass('active');
+
+			var data = $(this).attr('data-filter');
+			$workGrid.isotope({
+				filter: data
+			});
+		});
+
+		if (document.getElementById('portfolio')) {
+			var $workGrid = $('.portfolio-grid').isotope({
+				itemSelector: '.all',
+				percentPosition: true,
+				masonry: {
+					columnWidth: '.grid-sizer'
+				}
+			});
+		}
+	});
+
+	/*----------------------------------------------------*/
+	/* Start Magnific Pop Up
+	/*----------------------------------------------------*/
+	if ($('.img-gal').length > 0) {
+		$('.img-gal').magnificPopup({
+			type: 'image',
+			gallery: {
+				enabled: true
+			}
+		});
+	}
+	/*----------------------------------------------------*/
+	/*  End  Magnific Pop Up
+	/*----------------------------------------------------*/
+
+	/*----------------------------------------------------*/
+    /* counter js
+    /*----------------------------------------------------*/
+    // if(document.getElementById("features_counter")){
+    //     $('.counter').counterUp({
+    //         delay: 10,
+    //         time: 1000
+    //     });
+    // }
+
+})(jQuery);
